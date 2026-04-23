1331623d356711dc1449db29fda7215707429e45
susanacharya <susanacharya.sp@gmail.com>
2026-04-23 12:52:27 +0545

    Fix Not Found error: add root redirect and fix Render build command

--- backend/hospital_system/urls.py
+++ backend/hospital_system/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
